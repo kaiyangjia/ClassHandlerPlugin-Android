2a5759c2684dca910ecd7b4c43124fd6a466727b
jiakaiyang <kaiyangjia@163.com>
2018-01-05 17:55:50 +0800

    init repo

---
+++ class-handler-plugin/src/main/groovy/com/jiakiayang/plugin/classhandler/timelog/TimeLogTask.groovy
@@ -0,0 +1,16 @@
+package com.jiakiayang.plugin.classhandler.timelog;
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction;
+
+/**
+ * Created by jia on 2018/1/2.
+ */
+
+public class TimeLogTask extends DefaultTask {
+
+    @TaskAction
+    public onAction() {
+
+    }
+}
